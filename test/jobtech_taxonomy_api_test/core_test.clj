224f11c75b40b2ca33b483c210d3d49b34a3e4e0
qeshi <6067421+qeshi@users.noreply.github.com>
2020-04-08 15:34:44 +0200

    removed pmap

--- test/jobtech_taxonomy_api_test/core_test.clj
+++ test/jobtech_taxonomy_api_test/core_test.clj
@@ -2,69 +2,48 @@
   (:require [clojure.test :refer :all]
             [jobtech-taxonomy-api-test.core :refer :all]
             [clj-http.client :as client]
-            [cheshire.core :refer :all]
-            ))
-
+            [cheshire.core :refer :all]))
 
 (def api-key (System/getenv "JOBTECH_TAXONOMY_API_KEY"))
 
 (defn get-filename-v2-from-type [type]
-  (str "resources/" type "_v2.json")
-  )
+  (str "resources/" type "_v2.json"))
 
-
-(def url (str (System/getenv "JOBTECH_TAXONOMY_API_URL")  "v1/taxonomy/main/concepts") )
-
+(def url (str (System/getenv "JOBTECH_TAXONOMY_API_URL")  "v1/taxonomy/main/concepts"))
 
 (defn parse-local-preferred-labels [filename]
-  (map :term (parse-string (slurp filename) true )))
-
+  (map :term (parse-string (slurp filename) true)))
 
 (defn call-api [preferredLabel type url]
+
   (client/get url {:as :json-strict
-                   :headers {"api-key" api-key}
-   ;;                :debug true
+                   :headers {"api-key" api-key
+                             "Content-Type" "application/json"}
+                   ;;                :debug true
                    :query-params {"preferred-label" preferredLabel
-                                  "type" type
-                                  }})
-  )
+                                  "type" type}}))
 
 (defn get-concept [preferredLabel type url]
-  (:body (call-api preferredLabel type url))
-  )
-
+  (:body (call-api preferredLabel type url)))
 
 (defn get-remote-preferredLabel [preferredLabelLocal type url]
-  [preferredLabelLocal (:taxonomy/preferred-label (first (get-concept preferredLabelLocal type url)))]
-  )
-
+  [preferredLabelLocal (:taxonomy/preferred-label (first (get-concept preferredLabelLocal type url)))])
 
 (defn get-remote-and-local-values [type local-preferred-labels url]
-  (pmap #(get-remote-preferredLabel % type url) local-preferred-labels)
-  )
-
+  (map #(get-remote-preferredLabel % type url) local-preferred-labels))
 
 (defn is-local-equal-to-remote [type file url]
-  (let [
-        result (get-remote-and-local-values type (parse-local-preferred-labels file) url)]
+  (let [result (get-remote-and-local-values type (parse-local-preferred-labels file) url)]
 
-
-    (doall (pmap (fn [[expected actual]]
-                  (is (= expected actual))
-                  )  result ))
-    )
-  )
-
+    (doall (map (fn [[expected actual]]
+                  (is (= expected actual)))  result))))
 
 (defn test-taxonomy [name type file url]
   (testing name
-    (is-local-equal-to-remote type file url)
-    )
-  )
+    (is-local-equal-to-remote type file url)))
 
 (def types
-  [
-   "continent",
+  ["continent",
    "country",
    "driving-licence",
  ;;  "employment_duration",
@@ -96,51 +75,39 @@
 ;;   "sun_education_level_3",
 ;;   "wage_type",
 ;;   "worktime_extent"
-   ]
-  )
+   ])
 
 (def types-short
-  [
-   ;;"driving-licence"
+  [;;"driving-licence"
    ;;"language-level"
-   "country"
-   ]
-  )
+   "country"])
 
 (defn run-taxonomy-tests [types url]
-  (doall (pmap (fn [type]
+  (doall (map (fn [type]
                 (test-taxonomy (str type "--" url)
                                type
                                (get-filename-v2-from-type type)
                                url))
-              types
-              )))
-
+              types)))
 
 (deftest test-all
   (do
-    (run-taxonomy-tests types url)
-    )
-  )
-
+    (run-taxonomy-tests types url)))
 
 (defn v67-file []
-  (parse-string (slurp "resources/concept_to_taxonomy_v1.json") true ))
-
+  (parse-string (slurp "resources/concept_to_taxonomy_v1.json") true))
 
 (defn call-api-with-concept-id [concept-id url]
+
   (client/get url {:accept :json
-                   :as :auto
-                   :headers {"api-key" api-key}
+                   :as :json-strict
+                   :headers {"api-key" api-key
+                             "Content-Type" "application/json"}
                    :query-params {"id" concept-id
-                                  "version" 1
-                                  }})
-  )
+                                  "version" 1}}))
 
 (defn preferred-label-from-api-call [concept-id url]
-  (:taxonomy/preferred-label (first (:body (call-api-with-concept-id concept-id url))))
-  )
-
+  (:taxonomy/preferred-label (first (get (call-api-with-concept-id concept-id url) :body))))
 
 (defn has-unwanted-type [type]
   (contains? #{"place"
@@ -152,36 +119,23 @@
                "sun-education-level-1"
                "deprecated-education-field"
                "deprecated-education-level"
-               "occupation-experience-years"
-               } type)
-  )
+               "occupation-experience-years"} type))
 
 (defn call-api-with-v67-test-data [url]
-  (pmap (fn [[concept-id concept]]
+  (map (fn [[concept-id concept]]
          (let [expected (:label concept)
-               actual  (preferred-label-from-api-call (name concept-id) url)
-               ]
-           [expected actual]
-           )
+               actual  (preferred-label-from-api-call (name concept-id) url)]
+           [expected actual]))
 
-         )
-       (filter (fn [[id con]]   (not (has-unwanted-type (:type con)))  ) (v67-file) )
-
-       )
-  )
+       (doall (filter (fn [[id con]]   (not (has-unwanted-type (:type con)))) (v67-file)))))
 
 (defn is-local-equal-to-remote-v67 [url]
 
   (let [result (call-api-with-v67-test-data url)]
 
-    (doall (pmap (fn [[expected actual]]
-                  (is (= expected actual))
-                  )  result )))
-  )
-
+    (doall (map (fn [[expected actual]]
+                  (is (= expected actual)))  result))))
 
 (deftest test-version-67
   (testing "testing version 67 develop"
-    (is-local-equal-to-remote-v67 url)
-    )
-  )
+    (is-local-equal-to-remote-v67 url)))
